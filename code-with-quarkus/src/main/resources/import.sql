7763e546f86f8e221bc7b1e2d67746fdc3eee502
Yann Lucas <101208372+Y4nnLS@users.noreply.github.com>
2024-05-29 14:13:32 -0300

    delete files

--- code-with-quarkus/src/main/resources/import.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO movies(id,release_year,title,studios,producers,winner) VALUES (1,2020,'teste1','teste1','teste1',true);
-INSERT INTO movies(id,release_year,title,studios,producers,winner) VALUES (2,2020,'teste2','teste2','teste2',false);
